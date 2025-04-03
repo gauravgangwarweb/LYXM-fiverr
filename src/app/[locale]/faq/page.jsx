31300243b782721de1698ea33f96065ec0bb936e
gauravgangwarweb <gauravgangwar501@gmail.com>
2025-04-03 19:22:58 +0530

    Update layout in Invest and Inquiry pages; add FAQ page with content and translations

---
+++ src/app/[locale]/faq/page.jsx
@@ -0,0 +1,101 @@
+import Footer from "@/app/ui/footer/Footer";
+import GrayNavbarProvider from "@/app/ui/gray-navbar/GrayNavbarProvider";
+import { useTranslations } from "next-intl";
+
+
+const FAQ = () => {
+    const t = useTranslations("faq");
+  return (
+    <>
+      <GrayNavbarProvider />
+      <section className="bg-[#171818] w-full py-16 md:py-20 px-4 md:px-24">
+        <p className="text-[#f5f5f7] text-[26px] md:text-[56px]">
+          {t("title")}
+        </p>
+        {/* faq 1 */}
+        <div className="w-full flex flex-col md:flex-row border-t text-[#f5f5f7] border-white py-10 mt-4">
+            <div className="w-full md:w-1/2">
+                <p className="text-[20px] md:text-[34px]">{t("faq_1.question")}</p>
+            </div>
+            <div className="w-full md:w-1/2 text-base md:text-lg mt-6 md:mt-0">
+                <p className="">{t("faq_1.answer.paragraph_1")}</p>
+                <div className="mt-6">
+                    <p className="">{t("faq_1.answer.paragraph_2.width")}</p>
+                    <p className="">{t("faq_1.answer.paragraph_2.height")}</p>
+                    <p className="">{t("faq_1.answer.paragraph_2.depth")}</p>
+                </div>
+                <p className="mt-6">{t("faq_1.answer.paragraph_3")}</p>
+            </div>
+        </div>
+        {/* faq 2 */}
+        <div className="w-full flex flex-col md:flex-row border-t text-[#f5f5f7] border-white py-10 mt-4">
+            <div  className="w-full md:w-1/2">
+                <p className="text-[20px] md:text-[34px]">{t("faq_2.question")}</p>
+            </div>
+            <div className="w-full md:w-1/2 text-base md:text-lg mt-6 md:mt-0">
+                <p className="">{t("faq_2.answer.paragraph_1")}</p>
+                <div className="mt-6">
+                    <p className="">{t("faq_2.answer.paragraph_2")}</p>
+                </div>
+                <p className="mt-6">{t("faq_2.answer.paragraph_3")}</p>
+            </div>
+        </div>
+        {/* faq 3 */}
+        <div className="w-full flex flex-col md:flex-row border-t text-[#f5f5f7] border-white py-10 mt-4">
+            <div className="w-full md:w-1/2">
+                <p className="text-[20px] md:text-[34px]">{t("faq_3.question")}</p>
+            </div>
+            <div className="w-full md:w-1/2 text-base md:text-lg mt-6 md:mt-0">
+                <p className="">{t("faq_3.answer.paragraph_1")}</p>
+                <div className="mt-6">
+                    <p className="">{t("faq_3.answer.paragraph_2")}</p>
+                </div>
+                <p className="mt-6">{t("faq_3.answer.paragraph_3")}</p>
+            </div>
+        </div>
+        {/* faq 4 */}
+        <div className="w-full flex flex-col md:flex-row border-t text-[#f5f5f7] border-white py-10 mt-4">
+            <div className="w-full md:w-1/2 pr-0 md:pr-20">
+                <p className="text-[20px] md:text-[34px]">{t("faq_4.question")}</p>
+            </div>
+            <div className="w-full md:w-1/2 text-base md:text-lg mt-6 md:mt-0">
+                <p className="">{t("faq_4.answer.paragraph_1")}</p>
+            </div>
+        </div>
+        {/* faq 5 */}
+        <div className="w-full flex flex-col md:flex-row border-t text-[#f5f5f7] border-white py-10 mt-4">
+            <div className="w-full md:w-1/2">
+                <p className="text-[20px] md:text-[34px]">{t("faq_5.question")}</p>
+            </div>
+            <div className="w-full md:w-1/2 text-base md:text-lg mt-6 md:mt-0">
+                <p className="">{t("faq_5.answer.paragraph_1")}</p>
+                <p className="mt-6">{t("faq_5.answer.paragraph_2")}</p>
+            </div>
+        </div>
+        {/* faq 6 */}
+        <div className="w-full flex flex-col md:flex-row border-t text-[#f5f5f7] border-white py-10 mt-4">
+            <div className="w-full md:w-1/2">
+                <p className="text-[20px] md:text-[34px]">{t("faq_6.question")}</p>
+            </div>
+            <div className="w-full md:w-1/2 text-base md:text-lg mt-6 md:mt-0">
+                <p className="">{t("faq_6.answer.paragraph_1")}</p>
+                <p className="mt-6">{t("faq_6.answer.paragraph_2")}</p>
+            </div>
+        </div>
+        {/* faq 7 */}
+        <div className="w-full flex flex-col md:flex-row border-t text-[#f5f5f7] border-white py-10 mt-4">
+            <div className="w-full md:w-1/2">
+                <p className="text-[20px] md:text-[34px]">{t("faq_7.question")}</p>
+            </div>
+            <div className="w-full md:w-1/2 text-base md:text-lg mt-6 md:mt-0">
+                <p className="">{t("faq_7.answer.paragraph_1")}</p>
+                <p className="mt-6">{t("faq_7.answer.paragraph_2")}</p>
+            </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default FAQ;
